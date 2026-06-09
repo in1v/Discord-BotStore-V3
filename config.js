93d98cb3833554e3e3943cab157ca07d3286e458
Vinicius <65147463+in1v@users.noreply.github.com>
2026-06-09 17:50:31 -0300

    Configura deploy no Render

--- config.js
+++ config.js
@@ -13,5 +13,7 @@ module.exports = {
   panelPort: optional("PANEL_PORT", optional("PORT", "3000")),
   panelHost: optional("PANEL_HOST", process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1"),
   publicBaseUrl: optional("PUBLIC_BASE_URL"),
+  dataDir: optional("DATA_DIR"),
+  uploadDir: optional("UPLOAD_DIR"),
   panelPassword: optional("PANEL_PASSWORD")
 };
